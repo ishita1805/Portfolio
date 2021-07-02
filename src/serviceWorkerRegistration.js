4159015cb9d4bc9689887428d0909d5166d1b2f0
Ishita Kabra <ishitakabra1805@gmail.com>
2021-07-03 02:55:14 +0530

    TODO: cache busting

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -65,8 +65,8 @@ function registerValidSW(swUrl, config) {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
               // console.log("new version available. please reload")
-              console.log("new version available.. reloading")
-              window.location.reload();
+              console.log("new version available.. please reload")
+              // window.location.reload();
               // onUpdate callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
